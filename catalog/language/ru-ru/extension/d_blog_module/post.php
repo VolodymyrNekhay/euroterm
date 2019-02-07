845411367916747e86bb24a7011019c94cbb3dad
VolodymyrNekhayOSF <volodymyr.nekhay@osf-global.com>
2019-02-07 20:17:33 +0200

    blog-20 : basic translations.

---
+++ catalog/language/ru-ru/extension/d_blog_module/post.php
@@ -0,0 +1,18 @@
+<?php
+// Text
+$_['text_search']              = 'Поиск';
+
+
+$_['text_posted_by']           = 'Posted by';
+$_['text_anonymous']           = 'Anonymous';
+$_['text_on']                  = 'on';
+$_['text_edit']                = 'Edit';
+
+$_['text_tags']                = 'Теги:';
+$_['text_error']               = 'Статья не найдена!';
+$_['text_payment_recurring']   = 'Payment Profile';
+$_['text_day']                 = 'день';
+$_['text_week']                = 'неделя';
+$_['text_semi_month']          = 'half-month';
+$_['text_month']               = 'месяц';
+$_['text_year']                = 'год';
---
+++ catalog/language/ua-uk/extension/d_blog_module/post.php
@@ -0,0 +1,18 @@
+<?php
+// Text
+$_['text_search']              = 'Пошук';
+
+
+$_['text_posted_by']           = 'Posted by';
+$_['text_anonymous']           = 'Anonymous';
+$_['text_on']                  = 'on';
+$_['text_edit']                = 'Edit';
+
+$_['text_tags']                = 'Теги:';
+$_['text_error']               = 'Стаття не знайдена!';
+$_['text_payment_recurring']   = 'Payment Profile';
+$_['text_day']                 = 'день';
+$_['text_week']                = 'неділя';
+$_['text_semi_month']          = 'half-month';
+$_['text_month']               = 'місяць';
+$_['text_year']                = 'рік';
